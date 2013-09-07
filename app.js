0fe687527e6857d65d939d4536f58276cfc02163
Sahat Yalkabov <sakhat@gmail.com>
2013-09-07 10:34:37 -0400

    Added facebook authentication

--- app.js
+++ app.js
@@ -1,15 +1,87 @@
-
-/**
- * Module dependencies.
- */
-
 var express = require('express');
-var routes = require('./routes');
-var user = require('./routes/user');
 var http = require('http');
 var path = require('path');
+var mongoose = require('mongoose');
+var passport = require('passport');
+var FacebookStrategy = require('passport-facebook').Strategy;
+
+// Routes
+var routes = require('./routes');
+var user = require('./routes/user');
 
 var app = express();
+
+// Connect to MongoDB
+mongoose.connect('mongodb://sahat:shoplook@ds043368.mongolab.com:43368/shoplook', function(err) {
+  if (err) {
+    console.error('Error connecting to database');
+    process.exit();
+  } else {
+    console.info('Database connection established');
+  }
+});
+
+// User model
+var User = mongoose.model('User', new mongoose.Schema({
+  fbId: { type: String, index: { unique: true } },
+  accessToken: String,
+  displayName: String,
+  first_name: String,
+  last_name: String,
+  username: String,
+  link: String,
+  gender: String
+}));
+
+passport.serializeUser(function(user, done) {
+  done(null, user.fbId);
+});
+
+passport.deserializeUser(function(fbId, done) {
+  User.findOne({ 'fbId': fbId }, function(err, user) {
+    done(err, user);
+  });
+});
+
+// Facebook Strategy Configuration
+passport.use(new FacebookStrategy({
+    clientID: '159067527627361',
+    clientSecret: '41fba018b8986895be495d74922fae7e',
+    callbackURL: "http://shoplook.com:3000/auth/facebook/callback"
+  },
+  function(accessToken, refreshToken, profile, done) {
+    process.nextTick(function () {
+      User.findOne({ 'googleId': profile.id }, function(err, existingUser) {
+        if(existingUser) {
+          console.log('User: ' + existingUser.displayName + ' found and logged in!');
+          done(null, existingUser);
+        } else {
+          console.log(accessToken);
+          console.log(profile);
+          var newUser = new User({
+            fbId: profile.id,
+            accessToken: accessToken,
+            displayName: profile.displayName,
+            first_name: profile._json.first_name,
+            last_name: profile._json.last_name,
+            username: profile._json.username,
+            link: profile._json.link,
+            gender: profile._json.gender
+          });
+          newUser.save(function(err) {
+            if(err) return err;
+            console.log('New user: ' + newUser.displayName + ' created and logged in!');
+            done(null, newUser);
+          });
+        }
+      });
+    });
+  }
+));
+
+
+
+
 
 // all environments
 app.set('port', process.env.PORT || 3000);
@@ -19,6 +91,10 @@ app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.bodyParser());
 app.use(express.methodOverride());
+app.use(express.cookieParser());
+app.use(express.session({ secret: 'keyboard cat' }));
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -27,8 +103,29 @@ if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
+function ensureAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) { return next(); }
+  res.redirect('/login');
+}
+
+app.get('/auth/facebook', passport.authenticate('facebook'));
+app.get('/auth/facebook/callback', passport.authenticate('facebook', {
+    successRedirect: '/', failureRedirect: '/'
+}));
+
+app.get('/login', function(req, res) {
+ res.redirect('/');
+});
+
+app.get('/logout', function(req, res){
+  req.logout();
+  res.redirect('/');
+});
+
+
 app.get('/', routes.index);
 app.get('/users', user.list);
+
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
